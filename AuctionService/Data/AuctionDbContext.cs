aba6b99b725e32ee8410cb42b3f4199df110eef5
ExoDragon <to.terlaak@outlook.com>
2024-06-09 10:05:57 +0200

    file commit after .gitignore

---
+++ AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,22 @@
+using AuctionService.Entities;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    public DbSet<Item> Items { get; set; }
+    
+    public AuctionDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.AddInboxStateEntity();
+        modelBuilder.AddOutboxMessageEntity();
+        modelBuilder.AddOutboxStateEntity();
+    }
+}
---
+++ SearchService/Program.cs
@@ -0,0 +1,51 @@
+using System.Net;
+using MassTransit;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Consumers;
+using SearchService.Data;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitilazer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+ => HttpPolicyExtensions
+     .HandleTransientHttpError()
+     .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+     .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+     
